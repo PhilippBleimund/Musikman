c40b881ace7b525cb2674d710d1cd956135d6a71
Philipp Bleimund <maginir.philipp@gmail.com>
2021-10-05 22:43:58 +0200

    custom join sounds. Able to load and play local files

--- src/main/java/Dinkel/Musikman/Information.java
+++ src/main/java/Dinkel/Musikman/Information.java
@@ -7,6 +7,8 @@ public class Information {
 
 	public static String Token = "ODg4NDQwMjc5NjcyMTgwNzY2.YUSufQ.IxTf_QecGnEyQz8FBef6ijSniI8";
 	
+	public static String TokenPhilipp = "NDA2NTQwNzAxNzYyMDYwMzE4.X_n6GQ.GgbJhlYASP1X8zuuJ687NDUJtPY";
+	
 	public static long[] admins = new long[]{
 		406540701762060318l,	//Philipp Bleimund
 		395012173702758411l		//Simon Krampe
--- src/main/java/Dinkel/Musikman/Lavaplayer/PlayerManager.java
+++ src/main/java/Dinkel/Musikman/Lavaplayer/PlayerManager.java
@@ -1,21 +1,32 @@
 package Dinkel.Musikman.Lavaplayer;
 
-import java.util.Map;
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.source.bandcamp.BandcampAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.beam.BeamAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.http.HttpAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.local.LocalAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.soundcloud.SoundCloudAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.twitch.TwitchStreamAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.vimeo.VimeoAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioReference;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 
 import Dinkel.Musikman.Manager.TicketManager;
 import Dinkel.Musikman.Tickets.playlistTicket;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.VoiceChannel;
 
 public class PlayerManager {
 
@@ -23,13 +34,20 @@ public class PlayerManager {
 	
 	private Map<Long, GuildMusicManager> musicManagers;
 	private AudioPlayerManager audioPlayerManager;
+	private LocalAudioSourceManager localSourceManager;
 	
 	public PlayerManager() {
 		this.musicManagers = new HashMap<>();
 		this.audioPlayerManager = new DefaultAudioPlayerManager();
 		
-		AudioSourceManagers.registerRemoteSources(this.audioPlayerManager);
-		AudioSourceManagers.registerLocalSource(this.audioPlayerManager);
+		audioPlayerManager.registerSourceManager(new YoutubeAudioSourceManager());
+		audioPlayerManager.registerSourceManager(SoundCloudAudioSourceManager.createDefault());
+		audioPlayerManager.registerSourceManager(new BandcampAudioSourceManager());
+		audioPlayerManager.registerSourceManager(new VimeoAudioSourceManager());
+		audioPlayerManager.registerSourceManager(new TwitchStreamAudioSourceManager());
+		audioPlayerManager.registerSourceManager(new BeamAudioSourceManager());
+		audioPlayerManager.registerSourceManager(new HttpAudioSourceManager());
+		audioPlayerManager.registerSourceManager(new LocalAudioSourceManager());
 	}
 	
 	public GuildMusicManager getMusikManager(Guild guild) {
@@ -40,6 +58,49 @@ public class PlayerManager {
 
             return guildMusicManager;
         });
+	}
+	
+	/**
+	 * No response to any channel
+	 * 
+	 * @param voice
+	 * @param location
+	 */
+	public void loadLocalFileSilent(VoiceChannel voice, File location) {
+		GuildMusicManager musicManager = this.getMusikManager(voice.getGuild());
+		
+		//LocalAudioSourceManager source = this.audioPlayerManager.source(LocalAudioSourceManager.class);
+		//source.loadItem(audioPlayerManager, new AudioReference(location.getAbsolutePath(), "local"));
+		
+		this.audioPlayerManager.loadItem(new AudioReference(location.getAbsolutePath(), "local"), new AudioLoadResultHandler() {
+
+			@Override
+			public void trackLoaded(AudioTrack track) {
+				musicManager.scheduler.queue(track);
+				System.out.println("join Sound played");
+			}
+
+			@Override
+			public void playlistLoaded(AudioPlaylist playlist) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void noMatches() {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void loadFailed(FriendlyException exception) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+		//this.audioPlayerManager.loadItemOrdered(musicManager,new AudioReference(location.getAbsolutePath(), "local"), null);
 	}
 	
 	public void loadAndPlay(TextChannel channel, String trackURL) {
