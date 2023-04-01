830a780d32bc3a3c1d83a5f814ef6a80f1ad7689
Philipp Bleimund <maginir.philipp@gmail.com>
2023-04-01 16:27:21 +0200

    updated join Command

--- src/main/java/Dinkel/Musikman/Commands/Music/join.java
+++ src/main/java/Dinkel/Musikman/Commands/Music/join.java
@@ -7,33 +7,50 @@ import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
+import net.dv8tion.jda.api.events.Event;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.managers.AudioManager;
+import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
+import net.dv8tion.jda.api.utils.messages.MessageCreateData;
 
 public class join extends Command {
-	public void commandCode(MessageReceivedEvent eventMessage, List<String> args, boolean publicExec) {
-		TextChannel channel = eventMessage.getChannel().asTextChannel();
+	public void textImplementation(MessageReceivedEvent eventMessage, List<String> args, boolean publicExec) {
 		Member self = eventMessage.getGuild().getSelfMember();
+		Member member = eventMessage.getMember();
+		AudioManager audioManager = eventMessage.getGuild().getAudioManager();
+
+		mergedImplementation(self, member, audioManager, new ImplementationData(eventMessage, publicExec, InvokeMethod.TEXT));
+	}
+
+	@Override
+	public void slashImplementation(SlashCommandInteractionEvent eventMessage, boolean publicExec) {
+		Member self = eventMessage.getGuild().getSelfMember();
+		Member member = eventMessage.getMember();
+		AudioManager audioManager = eventMessage.getGuild().getAudioManager();
+		
+		mergedImplementation(self, member, audioManager, new ImplementationData(eventMessage, publicExec, InvokeMethod.SLASH));
+	}
+
+	private void mergedImplementation(Member self, Member member, AudioManager audioManager, ImplementationData data){
 		GuildVoiceState selfVoiceState = self.getVoiceState();
 		
 		if(selfVoiceState.inAudioChannel()) {
-			this.publicExec(publicExec, () -> {channel.sendMessage("Im already in a voice channel").queue();});
+			this.publicExec(data.publicExec, this.createMessageRunnable(data.event, new MessageCreateBuilder().setContent("Im already in a voice channel").build(), data.type));
 			return;
 		}
-		
-		Member member = eventMessage.getMember();
+
 		GuildVoiceState memberVoiceState = member.getVoiceState();
 		
 		if(!memberVoiceState.inAudioChannel()) {
-			this.publicExec(publicExec, () -> {channel.sendMessage("You are not in a channel").queue();});
+			this.publicExec(data.publicExec, this.createMessageRunnable(data.event, new MessageCreateBuilder().setContent("You are not in a channel").build(), data.type));
 			return;
 		}
 		
-		AudioManager audioManager = eventMessage.getGuild().getAudioManager();
 		VoiceChannel memberChannel = memberVoiceState.getChannel().asVoiceChannel();
 		
 		audioManager.openAudioConnection(memberChannel);
-		this.publicExec(publicExec, () -> {channel.sendMessage("Connecting to \uD83D\uDD0A" + memberChannel.getName()).queue();});
+		this.publicExec(data.publicExec, this.createMessageRunnable(data.event, new MessageCreateBuilder().setContent("Connecting to \uD83D\uDD0A" + memberChannel.getName()).build(), data.type));
 	}
 
 	@Override
