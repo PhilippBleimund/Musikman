07b547bf94c0921002e4f3c7e77aa26430458693
Philipp Bleimund <maginir.philipp@gmail.com>
2023-04-01 15:46:09 +0200

    implement Method to generate reply runnable

--- src/main/java/Dinkel/Musikman/Manager/Command.java
+++ src/main/java/Dinkel/Musikman/Manager/Command.java
@@ -2,12 +2,21 @@ package Dinkel.Musikman.Manager;
 
 import java.util.List;
 
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.Event;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.utils.messages.MessageCreateData;
 
 public abstract class Command {
+
+	public enum InvokeMethod{
+		TEXT,
+		SLASH,
+		SLASH_DELAY
+	}
 
 	public abstract void textImplementation(MessageReceivedEvent  eventMessage, List<String> args, boolean publicExec);
 
@@ -18,6 +27,26 @@ public abstract class Command {
 			r.run();
 	}
 	
+	protected Runnable createMessageRunnable(Event event, MessageCreateData message, InvokeMethod method){
+		return createMessageRunnable(event, message, method, false);
+	}
+
+	protected Runnable createMessageRunnable(Event event, MessageCreateData message, InvokeMethod method, boolean slashGlobal){
+		switch(method){
+			case TEXT:
+				MessageReceivedEvent textEvent = (MessageReceivedEvent) event;
+				TextChannel channel = textEvent.getChannel().asTextChannel();
+				return () -> channel.sendMessage(message).queue();
+			case SLASH:
+				SlashCommandInteractionEvent slashEvent = (SlashCommandInteractionEvent) event;
+				return () -> slashEvent.reply(message).setEphemeral(slashGlobal).queue();
+			case SLASH_DELAY:
+				SlashCommandInteractionEvent slashDelayEvent = (SlashCommandInteractionEvent) event;
+				return () -> slashDelayEvent.getHook().sendMessage(message).setEphemeral(slashGlobal).queue();
+		}
+		return () -> {};
+	}
+
 	public abstract String[] getNames();
 	
 	public abstract String[] getArgs();
