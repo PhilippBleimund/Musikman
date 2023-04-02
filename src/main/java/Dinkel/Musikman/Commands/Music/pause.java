b9ba5440249906844e612634e7e61c3062fcf91a
Philipp Bleimund <maginir.philipp@gmail.com>
2023-04-02 14:01:05 +0200

    updated pause command

--- src/main/java/Dinkel/Musikman/Commands/Music/pause.java
+++ src/main/java/Dinkel/Musikman/Commands/Music/pause.java
@@ -5,47 +5,62 @@ import java.util.List;
 import Dinkel.Musikman.Lavaplayer.GuildMusicManager;
 import Dinkel.Musikman.Lavaplayer.PlayerManager;
 import Dinkel.Musikman.Manager.Command;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
 
 public class pause extends Command{
 
 	@Override
-	public void commandCode(MessageReceivedEvent eventMessage, List<String> args, boolean publicExec) {
-		TextChannel channel = eventMessage.getChannel().asTextChannel();
-		Member self = eventMessage.getGuild().getSelfMember();
+	public void textImplementation(MessageReceivedEvent eventMessage, List<String> args, boolean publicExec) {
+		Guild guild = eventMessage.getGuild();
+		Member member = eventMessage.getMember();
+
+		mergedImplementation(guild, member, new ImplementationData(eventMessage, publicExec, InvokeMethod.TEXT));
+	}
+
+	@Override
+	public void slashImplementation(SlashCommandInteractionEvent eventMessage, boolean publicExec) {
+		Guild guild = eventMessage.getGuild();
+		Member member = eventMessage.getMember();
+
+		mergedImplementation(guild, member, new ImplementationData(eventMessage, publicExec, InvokeMethod.SLASH));
+	}
+
+	public void mergedImplementation(Guild guild, Member member, ImplementationData data){
+		Member self = guild.getSelfMember();
 		GuildVoiceState selfVoiceState = self.getVoiceState();
 		
 		if(!selfVoiceState.inAudioChannel()) {
-			this.publicExec(publicExec, () -> {channel.sendMessage("I need to be in a voice channel").queue();});
+			this.publicExec(data.publicExec, this.createMessageRunnable(data.event, new MessageCreateBuilder().setContent("I need to be in a voice channel").build(), data.type));
 			return;
 		}
 		
-		Member member = eventMessage.getMember();
 		GuildVoiceState memberVoiceState = member .getVoiceState();
 		
 		if(!memberVoiceState.inAudioChannel()) {
-			this.publicExec(publicExec, () -> {channel.sendMessage("You are not in a channel").queue();});
+			this.publicExec(data.publicExec, this.createMessageRunnable(data.event, new MessageCreateBuilder().setContent("You are not in a channel").build(), data.type));
 			return;
 		}
 		
 		if(!memberVoiceState.getChannel().equals(selfVoiceState.getChannel())) {
-			this.publicExec(publicExec, () -> {channel.sendMessage("we are not in the same voice channel").queue();});
+			this.publicExec(data.publicExec, this.createMessageRunnable(data.event, new MessageCreateBuilder().setContent("we are not in the same voice channel").build(), data.type));
 			return;
 		}
 		
-		GuildMusicManager musicManager = PlayerManager.getInstance().getMusikManager(eventMessage.getGuild());
+		GuildMusicManager musicManager = PlayerManager.getInstance().getMusikManager(guild);
 		
 		boolean currState = musicManager.audioPlayer.isPaused();
 		
 		musicManager.audioPlayer.setPaused(!currState);
 		
 		if(currState == false) {
-			this.publicExec(publicExec, () -> {channel.sendMessage("paused the track").queue();});
+			this.publicExec(data.publicExec, this.createMessageRunnable(data.event, new MessageCreateBuilder().setContent("paused the track").build(), data.type));
 		}else {
-			this.publicExec(publicExec, () -> {channel.sendMessage("resumed the track").queue();});
+			this.publicExec(data.publicExec, this.createMessageRunnable(data.event, new MessageCreateBuilder().setContent("resumed the track").build(), data.type));
 		}
 	}
 
@@ -61,7 +76,6 @@ public class pause extends Command{
 
 	@Override
 	public String[] getArgs() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
