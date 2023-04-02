6b3b4019c26bb2df7a1a57158bbe720c45fb4511
Philipp Bleimund <maginir.philipp@gmail.com>
2023-04-02 13:26:36 +0200

    updated leave Command

--- src/main/java/Dinkel/Musikman/Commands/Music/leave.java
+++ src/main/java/Dinkel/Musikman/Commands/Music/leave.java
@@ -5,49 +5,62 @@ import java.util.List;
 import Dinkel.Musikman.Lavaplayer.GuildMusicManager;
 import Dinkel.Musikman.Lavaplayer.PlayerManager;
 import Dinkel.Musikman.Manager.Command;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.managers.AudioManager;
+import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
 
 public class leave extends Command{
 
 	@Override
-	public void commandCode(MessageReceivedEvent eventMessage, List<String> args, boolean publicExec) {
-		TextChannel channel = eventMessage.getChannel().asTextChannel();
-		Member self = eventMessage.getGuild().getSelfMember();
-		GuildVoiceState selfVoiceState = self.getVoiceState();
+	public void textImplementation(MessageReceivedEvent eventMessage, List<String> args, boolean publicExec) {
+		Guild guild = eventMessage.getGuild();		
+		Member member = eventMessage.getMember();
+
+		mergedImplementation(guild, member, new ImplementationData(eventMessage, publicExec, InvokeMethod.TEXT));
+	}
+
+	@Override
+	public void slashImplementation(SlashCommandInteractionEvent eventMessage, boolean publicExec) {
+		Guild guild = eventMessage.getGuild();	
+		Member member = eventMessage.getMember();
+
+		mergedImplementation(guild, member, new ImplementationData(eventMessage, publicExec, InvokeMethod.SLASH));
+	}
+
+	public void mergedImplementation(Guild guild, Member member, ImplementationData data){
+		GuildVoiceState selfVoiceState = guild.getSelfMember().getVoiceState();
 		
 		if(!selfVoiceState.inAudioChannel()) {
-			this.publicExec(publicExec, () -> {channel.sendMessage("I need to be in a voice channel").queue();});
+			this.publicExec(data.publicExec, this.createMessageRunnable(data.event, new MessageCreateBuilder().setContent("I need to be in a voice channel").build(), data.type));
 			return;
 		}
-		
-		Member member = eventMessage.getMember();
-		GuildVoiceState memberVoiceState = member .getVoiceState();
+
+		GuildVoiceState memberVoiceState = member.getVoiceState();
 		
 		if(!memberVoiceState.inAudioChannel()) {
-			this.publicExec(publicExec, () -> {channel.sendMessage("You are not in a channel").queue();});
+			this.publicExec(data.publicExec, this.createMessageRunnable(data.event, new MessageCreateBuilder().setContent("You are not in a channel").build(), data.type));
 			return;
 		}
 		
 		if(!memberVoiceState.getChannel().equals(selfVoiceState.getChannel())) {
-			this.publicExec(publicExec, () -> {channel.sendMessage("we are not in the same voice channel").queue();});
+			this.publicExec(data.publicExec, this.createMessageRunnable(data.event, new MessageCreateBuilder().setContent("We are not in the same voice channel").build(), data.type));
 			return;
 		}
 		
-		GuildMusicManager musicManager = PlayerManager.getInstance().getMusikManager(eventMessage.getGuild());
+		GuildMusicManager musicManager = PlayerManager.getInstance().getMusikManager(guild);
 		
 		musicManager.scheduler.repeating = false;
 		musicManager.scheduler.queue.clear();
 		musicManager.audioPlayer.stopTrack();
 		
-		AudioManager audioManager = eventMessage.getGuild().getAudioManager();
-		
+		AudioManager audioManager = guild.getAudioManager();
 		audioManager.closeAudioConnection();
 		
-		this.publicExec(publicExec, () -> {channel.sendMessage("I have left the voice channel").queue();});
+		this.publicExec(data.publicExec, this.createMessageRunnable(data.event, new MessageCreateBuilder().setContent("I have left the voice channel").build(), data.type));
 	}
 
 	@Override
